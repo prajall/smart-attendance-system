674b4944357f9149a9975e870b7cb0f042b35585
Prajal Maharjan <prajalmhrzn@gmail.com>
2024-11-25 22:37:45 +0545

    Send frames through websocket

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,49 +2,21 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import app from "./app";
 import { dbName } from "./constants";
+import http from "http";
+import { setupWebSocket } from "./websocket/socket";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017";
 
-// app.listen(PORT, async () => {
-//   console.log(`Server is running on port ${PORT}`);
-
-//   try {
-//     await mongoose.connect(`${MONGODB_URI}/${dbName}`);
-//     console.log("Database Connected Successfully");
-//   } catch (error) {
-//     console.error("Database Connection Error:", error);
-//   }
-// });
-
-// ============ Web Socket Server ================
-
-import http from "http";
-import { Server } from "socket.io";
-
+// Create the HTTP server
 const server = http.createServer(app);
 
-const io = new Server(server, {
-  cors: {
-    origin: ["http://localhost:3000"],
-    methods: ["GET", "POST"],
-  },
-});
+// Initialize WebSocket
+setupWebSocket(server);
 
-io.on("connection", (socket) => {
-  console.log("User connected to websocket: ", socket.id);
-
-  socket.on("message", (msg) => {
-    console.log("Message: ", msg);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("User disconnected");
-  });
-});
-
+// Start the server
 server.listen(3001, async () => {
   console.log("Server is running..");
   try {
